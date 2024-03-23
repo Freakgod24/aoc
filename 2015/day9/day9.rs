e9b5397fa11ee5f26efc945d4aae079a189918e0
Freakgod24 <dany.newby@gmail.com>
2024-03-23 00:29:05 -0400

    Day 9 - Improved

--- 2015/day9/day9.rs
+++ 2015/day9/day9.rs
@@ -1,4 +1,3 @@
-use fancy_regex::Regex;
 use itertools::Itertools;
 use std::collections::HashMap;
 use std::fs::File;
@@ -12,46 +11,37 @@ fn part1() {
     let file = File::open("./day9.txt").unwrap();
     let reader = BufReader::new(file);
 
-    let mut cities: Vec<String> = Vec::new();
-    let mut leg_distances: HashMap<String, i32> = HashMap::new();
-
-    let re1 = Regex::new(r"(\S+) to (\S+) = (\d+)").unwrap();
+    let mut distances: HashMap<String, HashMap<String, i32>> = HashMap::new();
 
     // Since the dataset is small, I will use a HashMap instead of a Graph.
     // The keys will be composed of the name of the cities.
     for line in reader.lines().flatten() {
-        let captures = re1.captures(&line).unwrap().unwrap();
+        let (src, _, dst, _, value) = line.split_whitespace().collect_tuple().unwrap();
+        let distance = value.parse().unwrap_or(0);
 
-        let city1 = captures[1].to_string();
-        let city2 = captures[2].to_string();
-        let leg_distance = captures[3].parse().unwrap_or(0);
+        distances
+            .entry(src.to_string())
+            .or_insert_with(HashMap::new)
+            .insert(dst.to_string(), distance);
 
-        if !cities.contains(&city1) {
-            cities.push(city1.clone());
-        }
-
-        if !cities.contains(&city2) {
-            cities.push(city2.clone());
-        }
-
-        let forward = format!("{}{}", city1, city2);
-        let reverse = format!("{}{}", city2, city1);
-        leg_distances.entry(forward).or_insert(leg_distance);
-        leg_distances.entry(reverse).or_insert(leg_distance);
+        distances
+            .entry(dst.to_string())
+            .or_insert_with(HashMap::new)
+            .insert(src.to_string(), distance);
     }
 
     // Again, considering that the dataset is relatively small, I will bruteforce the solution.
-    let k = cities.len();
-    let total_distances = cities.into_iter().permutations(k).map(|route| -> i32 {
-        route
-            .windows(2)
-            .map(|city| {
-                leg_distances
-                    .get(&format!("{}{}", city[0], city[1]))
-                    .unwrap()
-            })
-            .sum()
-    });
+    let k = distances.keys().count();
+    let total_distances = distances
+        .keys()
+        .into_iter()
+        .permutations(k)
+        .map(|route| -> i32 {
+            route
+                .windows(2)
+                .map(|city| distances[city[0]][city[1]])
+                .sum()
+        });
 
     let min_distance = total_distances.clone().min();
     let max_distance = total_distances.clone().max();
